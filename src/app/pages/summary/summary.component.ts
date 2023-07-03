7fb7aaddad2d65a99357b22d741d9cae82a7d319
VictorMattosOW <victor_rm@outlook.com>
2023-07-03 01:02:34 -0300

    criacao modal e getOrders

--- src/app/pages/summary/summary.component.ts
+++ src/app/pages/summary/summary.component.ts
@@ -1,50 +1,53 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Order } from 'src/app/shared/models/order.model';
+import { SessionService } from 'src/app/shared/services/session.service';
 import { AbstractComponent } from 'src/app/shared/utils/abstract.component';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  styleUrls: ['./summary.component.css'],
 })
-export class SummaryComponent extends AbstractComponent {
+export class SummaryComponent extends AbstractComponent implements OnInit{
   @ViewChild('dialog') dialogElement!: ElementRef<HTMLDialogElement>;
+  orderToEdit = {} as Order;
 
-  activeIndex: number | null = null;
-  orders: Order[] = [
-    {
-      id: '',
-      name: 'polenta',
-      price: 14.90,
-      quantity: 1,
-      sharedUsers: [{id: '', name: 'victor'}]
-    },
-    {
-      id: '',
-      name: 'polenta',
-      price: 14.90,
-      quantity: 1,
-      sharedUsers: [{id: '', name: 'victor'}]
-    },
-        {
-      id: '',
-      name: 'polenta',
-      price: 14.90,
-      quantity: 1,
-      sharedUsers: [{id: '', name: 'victor'}]
-    },
-  ];
+  orders: Order[] = [];
 
-  constructor() {
+  constructor(
+    private sessionService: SessionService,
+    private router: Router,
+  ) {
     super();
   }
 
-  openDialog(): void {
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.sessionService.getOrdersObservable().subscribe({
+      next: (orders: Order[]) => {
+        if(orders.length === 0) {
+          this.router.navigate(['registrar']);
+        }
+        this.orders = orders;
+      },
+    })
+  }
+
+  openDialog(order: Order): void {
+    this.orderToEdit = order;
     this.dialogElement.nativeElement.show();
   }
 
   closeDialog(): void {
+    this.orders.forEach((order) => {
+      if (order.id === this.orderToEdit.id) {
+        order = this.orderToEdit;
+      }
+    });
     this.dialogElement.nativeElement.close();
   }
-
 }
