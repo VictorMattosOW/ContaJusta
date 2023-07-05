01fbbfe4a41848187c08bb645447d95b415ac045
VictorMattosOW <victor_rm@outlook.com>
2023-07-04 22:52:34 -0300

    ajuste tela inicial

--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from 'src/app/shared/services/session.service';
 
@@ -7,17 +7,19 @@ import { SessionService } from 'src/app/shared/services/session.service';
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.css'],
 })
-export class StartComponent implements AfterViewInit {
+export class StartComponent implements AfterViewChecked {
   isXiaomiBrowser = /MiuiBrowser/i.test(navigator.userAgent);
   isSafariOnIphone =
     navigator.userAgent.includes('iPhone') &&
     navigator.userAgent.includes('Safari');
 
-  constructor(private sessionService: SessionService, private router: Router) {}
-  ngAfterViewInit(): void {
+  constructor(private sessionService: SessionService, private router: Router,private cd: ChangeDetectorRef) {}
+
+  ngAfterViewChecked() {
     setTimeout(() => {
       this.sessionService.setBackgroundColor('blue');
-    });
+    }, 0);
+    this.cd.detectChanges();
   }
 
   goToRegister() {
