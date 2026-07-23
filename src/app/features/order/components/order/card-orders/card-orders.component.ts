ec74a278b198c2a150c8d054e9d4da39fcb4a79d
Victor Mattos <victor_rm@outlook.com>
2026-07-23 17:51:07 -0300

    refactore(card-orders): remocao logica tooltip

--- src/app/features/order/components/order/card-orders/card-orders.component.ts
+++ src/app/features/order/components/order/card-orders/card-orders.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Order } from 'src/app/core/models/order.model';
-import { User } from 'src/app/core/models/user.model';
-import { UserService } from 'src/app/shared/services/user.service';
-import { multiplyValues } from 'src/app/shared/utils/utils';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Order } from 'app/core/models/order.model';
+import { APP_CONSTANTS } from 'app/shared/constants/app.constants';
 
 @Component({
   selector: 'app-card-orders',
@@ -10,42 +8,18 @@ import { multiplyValues } from 'src/app/shared/utils/utils';
   styleUrls: ['./card-orders.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardOrdersComponent implements OnInit {
+export class CardOrdersComponent {
   @Input() orders: Order[] = [];
   @Output() orderToDelete = new EventEmitter<Order>();
 
-  maxNumberOfUsersInDisplay = 0;
+  readonly constants = APP_CONSTANTS;
   tooltipDisplayStates: boolean[] = [];
-  multiplyValues = multiplyValues;
-  constructor(private userServices: UserService) {}
-
-  ngOnInit(): void {
-    this.maxNumberOfUsersInDisplay = this.userServices.maxNumberOfUsersInDisplayValue;
-  }
 
   trackByOrderId(index: number, order: Order): string {
     return order.id;
   }
 
-  getFormattedUserNamesForDisplay(users: User[]): string {
-    return this.userServices.getConcatenatedUserNames(users);
-  }
-
-  getMaxNumberOfUsersInDisplay(users: User[]): User[] {
-    return this.userServices.getMaxNumberOfUsersInDisplay(users);
-  }
-
   deleteItem(orderToDelete: Order) {
     this.orderToDelete.emit(orderToDelete);
-  }
-
-  // TODO: vai vazar daqui
-  onHover(index: number) {
-    this.tooltipDisplayStates[index] = true; // Define o estado de exibição do tooltip para o item de índice 'index' como true
-  }
-
-  // TODO: vai vazar daqui
-  onMouseout(index: number) {
-    this.tooltipDisplayStates[index] = false; // Define o estado de exibição do tooltip para o item de índice 'index' como false
   }
 }
